155cd3ec8d41119fe11b4f4211fbcc24a26daa9b
Steven Polido <stevendotpulido@gmail.com>
2021-03-23 22:48:34 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const highlightBtn = document.querySelector(".highlightBtn");
+const clearBtn = document.querySelector(".clearBtn");
+const numInput = document.querySelector(".numInput");
+const errorContainer = document.querySelector(".error-container");
+const numBox = Array.from(document.querySelectorAll(".numBox"));
+const numRegex = /^[1-9|*,]$/;
+
+highlightBtn.addEventListener("click", () => {
+  const val = numInput.value;
+  checkNum(val);
+});
+
+function checkNum(val) {
+  let error;
+  if (!numRegex.test(val)) {
+    error = "Please enter a number between 1 and 9";
+    errorContainer.style.display = "block";
+    errorContainer.innerHTML = error;
+  } else {
+    errorContainer.style.display = "none";
+    numBox.forEach((node, i) => {
+      node.classList.remove("marker-highlight");
+      if (node.textContent.indexOf(val) > -1) {
+        node.classList.add("marker-highlight");
+      }
+    });
+  }
+}
+
+clearBtn.addEventListener("click", () => {
+  numInput.value = "";
+
+  numBox.forEach((node, i) => {
+    node.classList.remove("marker-highlight");
+  });
+});
